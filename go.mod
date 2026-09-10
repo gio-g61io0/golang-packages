e17e304d09beb0ee357cb9246a56bdbf5b878ffb
Gio Gonzales <gio.gonzales@carsu.edu.ph>
2026-09-10 11:51:21 +0800

    probe sends email to my email

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module personal-http-server
 go 1.25.6
 
 require (
+	github.com/go-playground/validator/v10 v10.30.4
 	github.com/labstack/echo/v5 v5.3.1
 	github.com/stretchr/testify v1.12.0
 )
@@ -11,11 +12,13 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.15 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.30.4 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.5.0 // indirect
 	golang.org/x/crypto v0.55.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- health_checker/main.go
+++ health_checker/main.go
@@ -2,17 +2,97 @@ package main
 
 import (
 	"context"
+	"github.com/joho/godotenv"
+	gomail "gopkg.in/mail.v2"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"sync"
 	"syscall"
 	"time"
 )
 
 const BROKER_URL string = "https://uat.sindbad.tech/"
+const EMAIL_FROM string = "test@example.com"
+const EMAIL_TO string = "gio.gonzales@sindbad.tech"
+
+type Config struct {
+	BrokerUrl    string
+	EmailFrom    string
+	EmailTo      string
+	MailHost     string
+	MailPort     int
+	MailUsername string
+	MailPassword string
+}
+type Mail struct {
+	From    string
+	To      string
+	Subject string
+	Body    string
+}
+
+type MailManager struct {
+	dialer *gomail.Dialer
+}
+
+func (manager *MailManager) SendEmail(mail *Mail) error {
+
+	message := gomail.NewMessage()
+	message.SetHeader("From", mail.From)
+	message.SetHeader("Subject", mail.Subject)
+	message.SetHeader("To", mail.To)
+
+	if err := manager.dialer.DialAndSend(message); err != nil {
+		slog.Error("Error sending email", "Error", err)
+		return err
+	}
+
+	slog.Info("Email Sent Successfully", "To", mail.To)
+
+	return nil
+
+}
+
+func NewEmailManager(config *Config) MailManager {
+
+	return MailManager{
+		dialer: gomail.NewDialer(config.MailHost, config.MailPort, config.MailUsername, config.MailPassword),
+	}
+}
+
+func NewMail(from string, to string, subject string, body string) *Mail {
+	return &Mail{
+		From:    from,
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}
+}
+
+func Load() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	intPort, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		BrokerUrl:    os.Getenv("BROKER_URL"),
+		EmailFrom:    os.Getenv("EMAIL_FROM"),
+		EmailTo:      os.Getenv("EMAIL_TO"),
+		MailHost:     os.Getenv("MAIL_HOST"),
+		MailPort:     intPort,
+		MailUsername: os.Getenv("MAIL_USERNAME"),
+		MailPassword: os.Getenv("MAIL_PASSWORD"),
+	}, nil
+}
 
 type Client struct {
 	http *http.Client
@@ -29,7 +109,7 @@ func InitClient(timeout time.Duration, parentCtx *context.Context) *Client {
 
 }
 
-func (client *Client) HealthCheck() {
+func (client *Client) HealthCheck(healthCheckChan chan bool) {
 
 	req, err := http.NewRequestWithContext(*client.ctx, http.MethodGet, BROKER_URL, nil)
 	if err != nil {
@@ -41,14 +121,17 @@ func (client *Client) HealthCheck() {
 
 	if err != nil {
 		slog.Warn("Something went wrong reaching the broker", "err", err)
+		healthCheckChan <- true
 		return
 	}
 
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
+
 	if err != nil {
 		slog.Error("Error reading response body stream", "error", err)
+		healthCheckChan <- true
 	}
 
 	slog.Info("Broker Responded", "Status Code", resp.StatusCode, "Message", string(body))
@@ -56,6 +139,16 @@ func (client *Client) HealthCheck() {
 }
 
 func main() {
+	config, err := Load()
+
+	if err != nil {
+		slog.Error("Cannot load environment variables")
+		return
+	}
+
+	mailManager := NewEmailManager(config)
+
+	healthCheckChan := make(chan bool, 1)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 
@@ -75,14 +168,32 @@ func main() {
 			select {
 			case <-t.C:
 				slog.Info("Calling HealthCheck")
-				go client.HealthCheck()
+				go client.HealthCheck(healthCheckChan)
 
 			case <-sig:
 				slog.Info("Gracefully shutting down")
+				cancel()
 				return
 			}
 		}
 	}()
+
+	wg.Go(func() {
+		for {
+			select {
+			case val := <-healthCheckChan:
+				if val {
+					if err := mailManager.SendEmail(NewMail(config.EmailFrom, config.EmailTo, "Error Broker Area", "Test Body is here")); err != nil {
+						slog.Error("Something went wrong sending email", "Error", err)
+					}
+				}
+			case <-ctx.Done():
+				slog.Info("Gracefully shutting down in email sender routine")
+				return
+			}
+		}
+
+	})
 
 	wg.Wait()
 
